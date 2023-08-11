c457701039b0b8e579e798026d3953f9f8665bf1
iamtheluiz <iamtheluiz.dev@gmail.com>
2023-08-11 09:49:37 -0300

    feat: update projects and add cover link

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,7 +6,11 @@ import './styles.css';
 export default function Project({ project, inverted = false }) {
   return (
     <div className={`project-item ${inverted && 'inverted'}`}>
-      <img src={project.cover} alt={project.title} />
+      <div className="cover">
+        <a href={project.site} target="_blank" rel="noreferrer">
+          <img src={project.cover} alt={project.title} />
+        </a>
+      </div>
       <div className="details">
         <strong>{project.title}</strong>
         <p>{project.description}</p>
--- src/services/projects.js
+++ src/services/projects.js
@@ -7,17 +7,24 @@ module.exports = [
     site: 'https://iamtheluiz.github.io/gantt-organon'
   },
   {
+    title: '🗺️ Hydro Registry',
+    description: 'Site para cadastrar registros e hidrantes em um mapa interativo. Desenvolvido com React e Firebase.',
+    cover: 'https://raw.githubusercontent.com/iamtheluiz/hydro-registry/main/.github/home.png',
+    repository: 'https://github.com/iamtheluiz/hydro-registry',
+    site: 'https://hydro-register.web.app/'
+  },
+  {
+    title: '📽️ Cinemais',
+    description: 'Gerencie cinemas e sessões de filmes de modo prático e digital!',
+    cover: 'https://raw.githubusercontent.com/iamtheluiz/cinemais-admin/main/docs/screenshots/index.png',
+    repository: 'https://github.com/iamtheluiz/cinemais-admin',
+    site: 'https://cinemais-admin.vercel.app/'
+  },
+  {
     title: '🐯 Taiga',
     description: 'Taiga is a speech recognition app that executes registered voice commands. Created with React, Electron, DotNet and Typescript',
     cover: 'https://raw.githubusercontent.com/iamtheluiz/taiga/master/.github/taiga.png',
     repository: 'https://github.com/iamtheluiz/taiga',
     site: 'https://github.com/iamtheluiz/taiga'
-  },
-  {
-    title: '🗺️ Hydro Registry',
-    description: 'Site para cadastrar registros e hidrantes em um mapa interativo. Desenvolvido com React e Firebase.',
-    cover: 'https://raw.githubusercontent.com/iamtheluiz/hydro-registry/main/.github/home.png',
-    repository: 'https://github.com/iamtheluiz/hydro-registry',
-    site: 'hydro-register.web.app/'
   },
 ]
